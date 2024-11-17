9e2ea9c6d0aae59c78a9346db918a206380057d5
valdijunior <valdi.ads@gmail.com>
2024-11-17 17:02:22 -0300

    Projeto Tela de login.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Login from './Components/Login/Login'
 
 function App() {
 
   return (
     <div className='App'>
-      <p>Página de Login</p>
+      <Login/>
     </div>
   )
 }
